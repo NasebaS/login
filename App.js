e27f43343c67f6f6fed7ddd6b592ac95f3d7c732
Naseba Sathurudeen <naseba.hamee@gmail.com>
2023-07-06 05:06:57 -0700

    added

--- App.js
+++ App.js
@@ -1,24 +1,152 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './LoginScreen';
-import ForgotPassword from './ForgotPassword';
-import CreateAccount from './CreateAccount';
-import Home from './Home';
+import React, { useState } from 'react';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Stack = createStackNavigator();
 
 const App = () => {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Forgot Password" component={ForgotPassword} />
-          <Stack.Screen name="Create An Account" component={CreateAccount} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleLogin = () => {
+    
+    Alert.alert('Successfully logged in');
   };
-  
-  export default App;
-  
+
+  return (
+    <LinearGradient
+      colors={['#E70E79', '#5A36CE']}
+      start={{ x: 0, y: 0.5 }}
+      end={{ x: 1, y: 0.5 }}
+      style={styles.container}
+    >
+      <View style={styles.container}>
+      <Image
+        source={require('D:/Naseba/login/android/assets/images.jpg')}
+        style={{ width: 100, height: 100,marginBottom:20 }}
+      />
+       <Text style={styles.title}>MobileApp</Text>
+       <View style={styles.viewinput}>
+       <Icon name="user" size={30} color="#fff" />
+
+       <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={(text) => setUserName(text)}
+        />
+       </View>
+        <View style={styles.viewinput}>
+        <Icon name="user" size={30} color="#fff" />        
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity
+            style={[styles.checkbox, rememberMe && styles.checkboxChecked]}
+            onPress={() => setRememberMe(!rememberMe)}
+          />
+          <Text>Remember me</Text>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.link}>Forgot password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          {/* <LinearTextGradient style={{ fontWeight: "bold", fontSize: 72 }}
+  locations={[0, 1]}
+  colors={["red", "blue"]}
+  start={{ x: 0, y: 0 }}
+  end={{ x: 1, y: 0 }}> */}
+    <Text style={styles.gradientText}>Login</Text>
+{/* </LinearTextGradient> */}
+          </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.text}>Not a member?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={[styles.text, styles.link]}>Create Account</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: 'white',
+  },
+  input: {
+    width: 250,
+    height: 40,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderRadius: 14,
+    padding: 8,
+    backgroundColor: 'transparent',
+    color: 'white',
+    borderColor: 'white',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderRadius: 4,
+    borderWidth: 1,
+    marginRight: 8,
+  },
+  checkboxChecked: {
+    backgroundColor: 'blue',
+    borderColor: 'blue',
+  },
+  link: {
+    textDecorationLine: 'underline',
+    color: 'white',
+  },
+  button: {
+    width: 250,
+    backgroundColor: 'white',
+    borderRadius: 14,
+    padding: 12,
+    alignItems: 'center',
+    marginTop: 16,
+    color:'blue'
+  },
+  gradientText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    overflow:'hidden'
+  },
+  text: {
+    color: 'black',
+  },
+ 
+});
+
+export default App;
--- CreateAccount.js
+++ CreateAccount.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
 
-const CreateAccount = () => {
-  return (
-    <View>
-      <Text>CreateAccount</Text>
-    </View>
-  )
-}
+// const CreateAccount = () => {
+//   return (
+//     <View>
+//       <Text>Create Your Account here</Text>
+//     </View>
+//   )
+// }
 
-export default CreateAccount
+// export default CreateAccount
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({})
